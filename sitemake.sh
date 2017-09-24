062182ae3a0489d230d83ca03eeb123282cab36c
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2017-09-24 03:53:13 -0700

    Fix script.js not being created

--- sitemake.sh
+++ sitemake.sh
@@ -69,16 +69,18 @@ if [[ ${choice} = 2 ]]; then
   echo -e "<!DOCTYPE html>\n<head>\n<meta charset=\"UTF-8\">\n<link rel=\"stylesheet\" href=\"./css/styles.css\">\n<!-- Latest compiled and minified CSS -->
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n<!-- Optional theme -->
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n<!-- Latest compiled and minified JavaScript -->
-<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n<body>\n\n</body>\n\n</html>" >> ${sitename}/index.html
+<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n<body>\n\n<script src=\"./js/script.js\"></script>\n</body>\n\n</html>" >> ${sitename}/index.html
 
 # Create and echo directories and files
 mkdir ${sitename}/js ${sitename}/images ${sitename}/css
 echo -e "/* A brand new CSS file for ${sitename}*/\n/* Delete this comment */" >> ${sitename}/css/styles.css
+echo -e "// A brand new JS file for ${sitename}\n// Delete this comment" >> ${sitename}/js/script.js
 echo -e "/${sitename}/ directory was created successfully!"
 echo -e "/${sitename}/css directory was created successfully!"
 echo -e "/${sitename}/css/styles.css file was created successfully!"
 echo -e "/${sitename}/images directory was created successfully!"
 echo -e "/${sitename}/js directory was created successfully!"
+echo -e "/${sitename}/js/script.js file was created successfully!"
 echo -e "/${sitename}/index.html file was created successfully!"
 echo -e "/${sitename}/index.html was given a default HTML template"
 echo -e "The Bootstrap CDN & ${sitename}/css/styles.css links were embedded into ${sitename}/index.html"
@@ -91,16 +93,18 @@ echo -e "<!DOCTYPE html>\n<head>\n<meta charset=\"UTF-8\">\n<link rel=\"styleshe
 <script src=\"https://code.jquery.com/jquery-3.1.1.js\" integrity=\"sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=\" crossorigin=\"anonymous\"></script>\n<!-- Latest compiled and minified CSS -->
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n<!-- Optional theme -->
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n<!-- Latest compiled and minified JavaScript -->
-<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n<body>\n\n</body>\n\n</html>" >> ${sitename}/index.html
+<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n<body>\n\n<script src=\"./js/script.js\"></script>\n</body>\n\n</html>" >> ${sitename}/index.html
 
 # Create and echo directories and files
 mkdir ${sitename}/js ${sitename}/images ${sitename}/css
 echo -e "/* A brand new CSS file for ${sitename}*/\n/* Delete this comment */" >> ${sitename}/css/styles.css
+echo -e "// A brand new JS file for ${sitename}\n// Delete this comment" >> ${sitename}/js/script.js
 echo -e "/${sitename}/ directory was created successfully!"
 echo -e "/${sitename}/css directory was created successfully!"
 echo -e "/${sitename}/css/styles.css file was created successfully!"
 echo -e "/${sitename}/images directory was created successfully!"
 echo -e "/${sitename}/js directory was created successfully!"
+echo -e "/${sitename}/js/script.js file was created successfully!"
 echo -e "/${sitename}/index.html file was created successfully!"
 echo -e "/${sitename}/index.html was given a default HTML template"
 echo -e "The Bootstrap CDN, Jquery CDN & ${sitename}/css/styles.css links were embedded into ${sitename}/index.html"
@@ -108,16 +112,18 @@ echo -e "ENJOY!!"
 
 elif [[ ${choice} = 1 ]]; then
   # Write to index.html
-  echo -e "<!DOCTYPE html>\n<head>\n<meta charset=\"UTF-8\">\n<link rel=\"stylesheet\" href=\"./css/styles.css\">\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n<body>\n\n</body>\n\n</html>" >> ${sitename}/index.html
+  echo -e "<!DOCTYPE html>\n<head>\n<meta charset=\"UTF-8\">\n<link rel=\"stylesheet\" href=\"./css/styles.css\">\n\n<!-- Page starts here -->\n\n<title>Title of the document</title>\n</head>\n\n\n<body>\n<script src=\"./js/script.js\"></script>\n</body>\n\n</html>" >> ${sitename}/index.html
 
   # Create and echo directories and files
   mkdir ${sitename}/js ${sitename}/images ${sitename}/css
   echo -e "/* A brand new CSS file for ${sitename}*/\n/* Delete this comment */" >> ${sitename}/css/styles.css
+  echo -e "// A brand new JS file for ${sitename}\n// Delete this comment" >> ${sitename}/js/script.js
   echo -e "/${sitename}/ directory was created successfully!"
   echo -e "/${sitename}/css directory was created successfully!"
   echo -e "/${sitename}/css/styles.css file was created successfully!"
   echo -e "/${sitename}/images directory was created successfully!"
   echo -e "/${sitename}/js directory was created successfully!"
+  echo -e "/${sitename}/js/script.js file was created successfully!"
   echo -e "/${sitename}/index.html file was created successfully!"
   echo -e "/${sitename}/index.html was given a default HTML template"
   echo -e "/${sitename}/css/styles.css link was embedded into ${sitename}/index.html"
